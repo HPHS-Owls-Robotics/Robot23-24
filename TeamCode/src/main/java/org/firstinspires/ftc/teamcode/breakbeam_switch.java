094c3e5f6c20e27166d14e3d81d7beda810202c5
teji11 <129212648+teji11@users.noreply.github.com>
2023-10-06 20:28:09 -0400

    Create breakbeam_switch.java
    sample code to test on oct 7 2023

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/breakbeam_switch.java
@@ -0,0 +1,61 @@
+/**
+ *
+ * Created by Maddie and Bria!, FTC Team 4962, The Rockettes
+ * version 1.0 Aug 22, 2016
+ *
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+/**
+ * Sample program to read data from a generic switch.
+ * The sensor must be plugged into an digital port
+ * configured on driver hub as "switch".
+ *
+ * Ex:
+ * Hall effect sensor: See https://www.adafruit.com/product/158
+ * Break Beam sensor: See https://www.amazon.com/Adafruit-Accessories-Break-Beam-Sensor/dp/B01BU6YBWU
+ */
+@TeleOp(name = "Sensor: Generic Switch", group = "Sensor")
+//@Disabled
+public class SensorGenericSwitch extends LinearOpMode {
+
+    /*
+     * Main loop
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        /*
+         * Initialize the hardware
+         */
+
+        DigitalChannel genericSwitch;
+
+        genericSwitch = hardwareMap.digitalChannel.get("switch");
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // is it on or off?
+
+            boolean isIntercepted = genericSwitch.getState();
+
+            String switchState;
+            if (isIntercepted) {
+                switchState = "Object Absent";
+            } else {
+                switchState = "Object Present";
+            }
+            telemetry.addData("time", "elapsed time: " + Double.toString(this.time));
+            telemetry.addData("state", ":  " + switchState);
+            telemetry.update();
+        }
+    }
+
+}
