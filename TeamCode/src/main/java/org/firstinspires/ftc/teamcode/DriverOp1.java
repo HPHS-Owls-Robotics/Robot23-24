bbffda7e7524abaac235ea9218f477cc1700e308
s-hello-s <104320427+s-hello-s@users.noreply.github.com>
2023-10-24 14:28:15 -0400

    Create DriverOp1.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOp1.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
+
+@TeleOp(name="Driving", group="12417")
+
+public class DriverOp1 extends LinearOpMode {
+    
+    DcMotor LMotor;
+    DcMotor RMotor;  
+    
+    LMotor = hardwareMap.dcMotor.get("motorLeft");
+    RMotor = hardwareMap.dcMotor.get("motorRight");
+        
+    //breakBeam = hardwareMap.digitalChannel.get("Beam");
+    
+    DcMotor[] motors = {LMotor, RMotor};
+        for (int i =0; i<2; i++)
+        {
+            motors[i].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            motors[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        LMotor.setDirection(DcMotor.Direction.REVERSE);
+        RMotor.setDirection(DcMotor.Direction.FORWARD);
+    
+    //DigitalChannel breakBeam;
+    float MaxSpeed = 1.0f;
+    @Override
+    
+    public void runOpMode() throws InterruptedException
+    {
+        waitForStart();
+        while (opModeIsActive()) {
+        
+        float LPwr=0, RPwr=0;
+        
+        
+        LPwr= gamepad1.left_stick_y*MaxSpeed+gamepad1.left_stick_x*MaxSpeed;
+        RPwr = gamepad1.left_stick_y*MaxSpeed- gamepad1.left_stick_x*MaxSpeed;
+
+        if(gamepad1.left_stick_y>0)
+        {
+            LPwr=MaxSpeed;
+            RPwr=MaxSpeed;
+
+        }
+        if(gamepad1.left_stick_y<0)
+        {
+            LPwr=-MaxSpeed;
+            RPwr=-MaxSpeed;
+
+        }
+        if(gamepad1.left_stick_x>0)
+        {
+            LPwr=MaxSpeed;
+            RPwr=-MaxSpeed;
+
+        }
+        if(gamepad1.left_stick_x<0)
+        {
+            LPwr=-MaxSpeed;
+            RPwr=MaxSpeed;
+        }
+
+        RMotor.setPower(RPwr);
+        LMotor.setPower(LPwr);
+        }
+
+    }
+}
