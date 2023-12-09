d7ec571447a0b12d18b25db474a6f2e68024d7ca
Tejasvi Venkat <teji.tulasi@gmail.com>
2023-12-08 23:45:53 -0500

    commit DriverOp2gamepads.java changes: - sweeper direction - mode 2 rr servo position - just some night-before comp changes - makeshift mode 1 w/o breakbeam function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOp2gamepads.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOp2gamepads.java
@@ -87,20 +87,25 @@ public class DriverOp2gamepads extends LinearOpMode {
                 //
             }
             if(gamepad1.b){
-                //sweep mode 1
-                if(!breakBeam.getState())
-                {
-                    rr.setPosition(2);
-                    telemetry.addLine("Beam intact");
-                } else
-                {
+//                //sweep mode 1
+//                if(!breakBeam.getState())
+//                {
+//                    rr.setPosition(2);
+//                    telemetry.addLine("Beam intact");
+//                } else
+//                {
+//                    rr.setPosition(0);
+//                    telemetry.addLine("Beam broke");
+//                }
+                if(rr.getPosition()==2){
                     rr.setPosition(0);
-                    telemetry.addLine("Beam broke");
+                } else {
+                    rr.setPosition(2);
                 }
             }
             if(gamepad1.a){
                 //sweep mode 2
-                rr.setPosition(0.52);
+                rr.setPosition(0.515);
             }
 
             if(gamepad1.dpad_up){
@@ -114,12 +119,12 @@ public class DriverOp2gamepads extends LinearOpMode {
 
             if(gamepad1.left_bumper){
                 //sweep out
-                Sweeper.setPower(-SPwr);
+                Sweeper.setPower(SPwr);
             }
             if(gamepad1.right_bumper)
             {
                 //sweep in
-                Sweeper.setPower(SPwr);
+                Sweeper.setPower(-SPwr);
             }
             if(gamepad1.left_bumper==gamepad1.right_bumper){
                 Sweeper.setPower(0);
@@ -129,7 +134,7 @@ public class DriverOp2gamepads extends LinearOpMode {
             if(gamepad2.x)
             {
                 //drone
-                drone.setPosition(2);
+                drone.setPosition(0);
             }
             if(gamepad2.y)
             {
@@ -142,7 +147,7 @@ public class DriverOp2gamepads extends LinearOpMode {
             if(gamepad2.b){
                 if(trapdoor.getPosition()==0.0){
                     trapdoor.setPosition(1.0);
-                    sleep(700);
+                    sleep(800);
                     trapdoor.setPosition(0.0);
                 } else{
                     trapdoor.setPosition(0.0);
